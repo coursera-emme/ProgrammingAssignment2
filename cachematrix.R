39007602529f92bb3e7fc4a363435a0a2a731872
Mattia <coursera-emme@users.noreply.github.com>
2014-08-10 11:36:41 +0200

    Comments and tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## `makeCacheMatrix` creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## `cacheSolve` computes the inverse of the special "matrix" returned by
+## `makeCacheMatrix` above, possibly using a cached value.
+## It assumes `x` is always invertible, the return value is a plain matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+# tests
+plain <- matrix(nrow=3, ncol=3, c(1,2,3,3,2,1,1,3,2))
+m <- makeCacheMatrix(plain)
+stopifnot(identical(m$get(), plain))
+m$set(NULL)
+stopifnot(identical(m$get(), NULL))
+m$set(plain)
+stopifnot(identical(m$get(), plain))
+
+im <- cacheSolve(m)
+imok <- solve(plain)
+stopifnot(identical(im, imok))
+im <- cacheSolve(m)
+stopifnot(identical(im, imok))
+
+
